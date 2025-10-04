0b54e037cdce6b46904c60e8ed3bc2d7c074e55b
DnvMaster <dnvcomp@yandex.ru>
2025-10-04 11:24:02 +0300

    Install template admin panel

---
+++ resources/views/dnvmaster/frontend/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('dnvmaster.frontend.admin.layouts.app')
+@section('title','DnvMaster - Администратор')
+@section('main')
+	<h2>DnvMaster - Администратор</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
 use App\Http\Controllers\HomeSectionController;
-use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\ProfileController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dnvmaster/frontend/index');
 });
 
-
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dnvmaster/frontend/admin/index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+*/
 
 require __DIR__.'/auth.php';
